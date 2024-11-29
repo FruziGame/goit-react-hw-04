5c49d840c561b30ea0a547e27085caf8c16b2e06
Hlib <grossogleb@gmail.coml>
2024-11-29 20:17:26 +0200

    'update'

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import SearchBar from "./components/SearchBar/SearchBar"
 import ImageGallery from "./components/ImageGallery/ImageGallery"
 import ImageModal from "./components/ImageModal/ImageModal";
 
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 
 
@@ -50,94 +50,73 @@ function App() {
 
 
 
-  const myApiKey = "28B1jJya0_5xgiojIju_h9eqIt0k8X8PK5RSk-HISFk"
+  const myApiKey = "hBjqH2luvWaYwVg60gWls-uptvg6osKZSiF3soX_8W8"
   axios.defaults.baseURL = "https://api.unsplash.com/search/photos/"
   axios.defaults.headers.common["Authorization"] = `Client-ID ${myApiKey}`;
 
-  const handleSubmit = async (input)=> {
+  const handleSubmit = (input) => {
 
-    try {
+    setInput(input)
+    setPage(1)
 
-      setError(false)
-      setLoading(true)
-
-      setData({
-        images: [],
-        total: 0,
-        total_pages: 0,
-      });
-      
-      const response = await  axios.get("", {
-        params: {
-          query: input,
-          per_page: 15,
-        }
-      })
-      setData(prevData => ({
-        ...prevData,
-        images: [...prevData.images, ...response.data.results],
-        total: response.data.total,
-        total_pages: response.data.total_pages,
-      }));
-
-      if (response.data.results.length === 0) {
-        toast.error("We couldn’t find any images matching your search. Try using different keywords.");
-      }
-
-      setInput(input)
-      setPage(2);
-
-      } catch (error) {
-        setError(true); 
-        toast.error("Oops! something went wrong, please try again later")
-
-      }
-      finally {
-        setLoading(false)
-    }
+    setData({
+      images: [],
+      total: 0,
+      total_pages: 0,
+    });
   }
 
+  useEffect(() => {
 
-  const loadMore = async () => {
-
-    try {
-
-      setError(false)
-      setLoading(true)
-
-      if (page > data.total_pages) {
-        throw new Error("No more pages");
-
+    if (input === "") {
+      return
     }
 
-      const response = await  axios.get("", {
-        params: {
-          query: input,
-          per_page: 4,
-          page: page,
+    async function fetchPhotos(input,page) {
+
+      try {
+
+        setError(false)
+        setLoading(true)
+  
+        const response = await  axios.get("", {
+          params: {
+            query: input,
+            per_page: 15,
+            page: page,
+          }
+        })
+
+
+        if (page > response.data.total_pages) {
+          toast.error("No more photos available for this search.");
+          return; 
         }
-      })
-      setData(prevData => ({
-        ...prevData,
-        images: [...prevData.images, ...response.data.results],
-        total: response.data.total,
-        total_pages: response.data.total_pages,
-      }));
 
-      setPage((prevPage) => prevPage + 1);
-
-
-    } catch (error) {
-      setError(true); 
-      toast.error("No more photos available for this search.")
+        setData(prevData => ({
+          ...prevData,
+          images: [...prevData.images, ...response.data.results],
+          total: response.data.total,
+          total_pages: response.data.total_pages,
+        }));
+  
+        } catch (error) {
+          setError(true); 
+          toast.error("Oops! something went wrong, please try again later")
+  
+        }
+        finally {
+          setLoading(false)
+      }
     }
-    finally {
-      setLoading(false)
-    }
+
+    fetchPhotos(input,page)
+  },[input,page])
+
+
+  const loadMore = () => {
+    setPage((prevPage) => prevPage + 1);
   }
-
-
-
 
 
   return (
@@ -149,7 +128,7 @@ function App() {
       { error && <Toaster />}
       {data.images.length > 0 &&   <button className={css.btn} onClick={loadMore}>Load more</button >}
       <ImageModal open={openModal} close={closeModal} isOpen={modalIsOpen} data={dataModal} />
-
+      
     </>
   )
 }
